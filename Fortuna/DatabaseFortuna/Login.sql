338e0bc50a2a23515d1b30a6b7459b2c38d2f45a
Dzm <kuks@repo.com>
2019-04-15 20:46:56 +0300

    Add projects

---
+++ Fortuna/DatabaseFortuna/Login.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Login]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Login] NCHAR(100) NOT NULL, 
+    [Mail] NCHAR(100) NOT NULL, 
+    [Password] NCHAR(50) NOT NULL
+)
---
+++ Fortuna/DatabaseFortuna/Profile.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Profile]
+(
+	[Id] INT NOT NULL , 
+    [Firstname] NCHAR(100) NOT NULL, 
+    [Lastname] NCHAR(100) NOT NULL, 
+	[Login] NCHAR(100) NOT NULL,
+    [Password] NCHAR(50) NOT NULL, 
+    [Mail] NCHAR(100) NOT NULL, 
+    [Balance] MONEY NULL, 
+    [WinnerIDs] NCHAR(30) NULL, 
+	[DateOfRegistration] DATETIME NOT NULL, 
+    PRIMARY KEY ([Id]), 
+)
